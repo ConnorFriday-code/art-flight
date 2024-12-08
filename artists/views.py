a5c6a6d8f200923bfabfba9c9b3aeba00d7705b6
Connor Friday <connor.friday.cf@gmail.com>
2024-12-08 04:25:56 +0000

    Creating artists.url and connecting it to the database.

--- artists/views.py
+++ artists/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Artist
 
 # Create your views here.
+
+def all_artists(request):
+
+    """ A view to return all artists, including sorting and searching """
+
+    artists = Artist.objects.all()
+
+    context = {
+        'artists': artists,
+    }
+
+    return render(request, 'artists/artists.html', context)
