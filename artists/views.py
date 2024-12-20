edd9ba3ca8c852fc22f8e2067804e483fd47443a
Connor Friday <connor.friday.cf@gmail.com>
2024-12-20 04:53:38 +0000

    Adding search function. Refining sorting layout to be clearer.

--- artists/views.py
+++ artists/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Artist
 
 # Create your views here.
@@ -9,11 +10,19 @@ def all_artists(request):
 
     sort_by = request.GET.get('sort',None)
     tag_filter = request.GET.get('tag',None)
+    search_query = request.GET.get('q',None)
 
     artists = Artist.objects.all()
 
     if tag_filter:
         artists = artists.filter(tag__name=tag_filter)
+    
+    if search_query:
+        artists =artists.filter(
+            Q(artist_name__icontains=search_query) |
+            Q(description__icontains=search_query) |
+            Q(dos__icontains=search_query)
+        )
 
     if sort_by=='price_low':
         artists = artists.order_by('price') 
@@ -24,6 +33,7 @@ def all_artists(request):
         'artists': artists,
         'tag':tag_filter,
         'sort': sort_by,
+        'search_query':search_query,
     }
 
     return render(request, 'artists/artists.html', context)
