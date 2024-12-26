264d3d74d921cd0b31e132075aef623bd031ee28
Connor Friday <connor.friday.cf@gmail.com>
2024-12-26 05:26:22 +0000

    Beginning to create user input form to create services.

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Artist(models.Model):
+    sku = models.CharField(max_length=10, unique=True)
+    artist_name = models.CharField(max_length=255, editable=False)
+    email = models.EmailField(editable=False)
+    description = models.TextField(blank=True)
+    dos = models.TextField(blank=True)
+    donts = models.TextField(blank=True)
+    image = models.ImageField(upload_to='artist_images/', blank=True, null=True)
+    price_options = models.JSONField(default=dict)
+    slots = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(50)])
+    tag = models.CharField(max_length=50, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='artists')
+
+    def save(self, *args, **kwargs):
+        """Automatically populate artist_name and email from user on save."""
+        if self.user:
+            self.artist_name = self.user.username
+            self.email = self.user.email
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.artist_name
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
 # Define the profile view
+@login_required
 def profile_view(request):
-    return render(request, 'profile/profile.html')
+    artist, created = Artist.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        artist.description = request.POST.get('description', artist.description)
+        artist.dos = request.POST.get('dos', artist.dos)
+        artist.donts = request.POST.get('donts', artist.donts)
+        artist.slots = request.POST.get('slots', artist.slots)
+        artist.price_options = request.POST.get('price_options', artist.price_options)
+        if 'image' in request.FILES:
+            artist.image = request.FILES['image']
+        artist.save()
+        return redirect('profile_view')
+
+    return render(request, 'profile/profile.html', {'artist': artist})
