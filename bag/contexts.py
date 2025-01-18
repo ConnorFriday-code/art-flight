1d983005fe09738134364b3a7a8c3193bdca6260
Connor Friday <connor.friday.cf@gmail.com>
2025-01-18 02:50:10 +0000

    Setting up bag content.

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,28 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from user_profile.models import Artist
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
+
+    bag = request.session.get('bag', {})
+
+    for artist_id, data in bag.items():
+        artist = get_object_or_404(Artist, id=artist_id)
+
+        for commission in data['commissions']:
+            total += commission['price']
+            bag_items.append({
+                'artist_id': artist_id,
+                'artist_name': artist.artist_name,
+                'commission_option': commission['commission_option'],
+                'price': commission['price'],
+                'details': commission['details'],
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
