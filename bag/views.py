60f64e8bbb1e8fb114a3e0995dad62b6318b9b84
Connor Friday <connor.friday.cf@gmail.com>
2025-01-18 06:10:38 +0000

    Creating modify bag popup and styling the pages a little.

--- bag/views.py
+++ bag/views.py
@@ -5,10 +5,33 @@ from user_profile.models import Artist
 # Create your views here.
 
 def bag(request):
+    """A view to return the bag page"""
+    bag = request.session.get('bag', {})
+    bag_items = []
+    total = 0
 
-    """ A view return the bag page """
+    price = {}
 
-    return render(request, 'bag/bag.html')
+    for artist_id, data in bag.items():
+        artist = get_object_or_404(Artist, id=artist_id)
+        price = artist.price
+
+        for commission in data['commissions']:
+            total += commission['price']
+            bag_items.append({
+                'artist_name': data['artist_name'],
+                'commission_option': commission['commission_option'],
+                'details': commission['details'],
+                'price': commission['price'],
+            })
+
+    context = {
+        'bag_items': bag_items,
+        'price': price,
+        'total': total,
+    }
+
+    return render(request, 'bag/bag.html', context)
 
 def add_to_bag(request, id):
     """Add a commission to the shopping bag"""
@@ -42,3 +65,28 @@ def add_to_bag(request, id):
     messages.success(request, f"Added a commission for {artist.artist_name} to your bag!")
 
     return redirect(redirect_url)
+
+def update_commission(request):
+
+    """Update a specific commission in the bag"""
+    if request.method == "POST":
+        bag = request.session.get('bag', {})
+        index = int(request.POST.get('index'))
+        artist_id = str(request.POST.get('artist_id'))
+        option = request.POST.get('commission_option')
+        details = request.POST.get('details')
+        price = float(request.POST.get('price'))
+
+        if artist_id in bag and 0 <= index < len(bag[artist_id]['commissions']):
+            bag[artist_id]['commissions'][index] = {
+                'commission_option': option,
+                'price': price,
+                'details': details,
+            }
+
+            request.session['bag'] = bag
+            messages.success(request, "Commission updated successfully!")
+        else:
+            messages.error(request, "Invalid commission data.")
+
+    return redirect('bag')
