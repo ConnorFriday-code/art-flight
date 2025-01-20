bb75556004e61992d722e89e59fcf7d5eda72c38
Connor Friday <connor.friday.cf@gmail.com>
2025-01-20 16:29:22 +0000

    Saving unsaved file from previous commit.

--- artists/views.py
+++ artists/views.py
@@ -22,20 +22,14 @@ def all_artists(request):
     tag_filter = request.GET.get('tag',None)
     search_query = request.GET.get('q',None)
 
-    #Grab all artists
     artists = Artist.objects.all()
 
-    #Open the json file and load tags
     with open('artists/fixtures/tags.json', 'r') as f:
         tags = json.load(f)
 
-    #Nav bar tag filter
     if tag_filter:
-        # Replace all space with nothing and lower case everything
         normalized_tag = tag_filter.strip().lower().replace(" ", "")
 
-        #Grab tag names and freindly names from json file
-        #Then remove all spaces and lowercase everything (future proof future tags)
         tag_names = [
             tag['fields']['name'].strip().lower().replace(" ", "") for tag in tags
         ]
@@ -43,17 +37,12 @@ def all_artists(request):
             tag['fields']['friendly_name'].strip().lower().replace(" ", "") for tag in tags
         ]
 
-        #Check if the normalized_tag matches either name or friendly_name
         if normalized_tag in tag_names or normalized_tag in friendly_names:
-            #Filter artists where the tag matches case-insensitively
             artists = artists.filter(
-                #Checks the artists' tags against the ones in the json file using exact inputs
                 Q(tag__iexact=tag_filter)
-                #Checks the artists' tags against the ones in the json file without spacing/capitals
                 | Q(tag__icontains=tag_filter)  
             )
-    
-    #Nav search bar filter
+
     if search_query:
         artists =artists.filter(
             Q(artist_name__icontains=search_query) |
@@ -61,10 +50,9 @@ def all_artists(request):
             Q(dos__icontains=search_query)
         )
 
-    #Sort artists by price, lowest first
     if sort_by=='price_low':
         artists = artists.order_by('price') 
-    #Sort artists by price, highest first
+
     elif sort_by=='price_high':
         artists = artists.order_by('-price')
 
