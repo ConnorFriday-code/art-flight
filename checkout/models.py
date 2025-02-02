9aa8b13aa4ea4c429300a09b400ebc8be26585e0
Connor Friday <connor.friday.cf@gmail.com>
2025-02-02 05:49:10 +0000

    Defining models and making migrations.

--- checkout/models.py
+++ checkout/models.py
@@ -19,11 +19,37 @@ class Order(models.Model):
     country = models.CharField(max_length=80, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_length=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_length=10, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        """ Generate a random number """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """ 
+        Update grand total each time a new line is added, 
+        accounting for delivery cost 
+        """
+        self.order_total = self.lineitems.aggregate(sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE/100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+    
+    def save(self, *args, **kwargs):
+        """ Override the original save method to set order number """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.order_number
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     artist = models.ForeignKey("user_profile.Artist", on_delete=models.CASCADE)
     commission_option = models.CharField(max_length=255, null=False, blank=False)
     details = models.TextField(blank=True, null=True)
@@ -32,3 +58,11 @@ class OrderLineItem(models.Model):
 
     def __str__(self):
         return f"{self.commission_option} from {self.artist.artist_name} in Order {self.order.order_number}"
+
+    def save(self, *args, **kwargs):
+        """ Override the original save method to set order number """
+        self.lineitem_total = self.artist.price
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.artist.sku} on order {self.order.order_number}'
