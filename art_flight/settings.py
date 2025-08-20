6de9a7ed089e22d665ff8d36580e63d2bdeebe9d
ConnorFriday-code <connor.friday.cf@gmail.com>
2025-08-20 08:57:58 +0100

    Fixing security issue of secret key being public, creating new secret key to replace old one.

--- art_flight/settings.py
+++ art_flight/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 """
 Django settings for art_flight project.
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^b!!$o&ou0!!zr)^7a$$-=^akijcd4a_6t+#*dwcbdjc4k+#cz'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-default")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -116,11 +117,15 @@ WSGI_APPLICATION = 'art_flight.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+ # DATABASES = {
+ #     'default': {
+ #         'ENGINE': 'django.db.backends.sqlite3',
+ #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #     }
+ # }
+     
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('')
 }
 
 
