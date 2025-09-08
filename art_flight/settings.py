a6c022ee7acbe8b8ada02b3a1ee6fbd207b7191a
ConnorFriday-code <connor.friday.cf@gmail.com>
2025-09-08 10:22:19 +0100

    Fixing AWS region name.

--- art_flight/settings.py
+++ art_flight/settings.py
@@ -191,7 +191,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'art-flight-90b83d1ec001'
-    AWS_S3_REGION_NAME = 'Europe (Stockholm) eu-north-1'
+    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
