14063d71f1cb85a21bbf304de3cd88a6fa8e03c1
ConnorFriday-code <connor.friday.cf@gmail.com>
2025-09-10 13:38:22 +0100

    Adding debug print message.

--- art_flight/settings.py
+++ art_flight/settings.py
@@ -211,3 +211,5 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'artflight@example.com'
+
+print(">>> STATICFILES_STORAGE =", STATICFILES_STORAGE if 'USE_AWS' in os.environ else "local")
