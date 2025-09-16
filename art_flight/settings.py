9d72df789667addb44eab8ab93ff015551b564c0
ConnorFriday-code <connor.friday.cf@gmail.com>
2025-09-16 03:25:22 +0100

    More print debugs.

--- art_flight/settings.py
+++ art_flight/settings.py
@@ -193,9 +193,6 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
-
-    print(f">>> [DEBUG] AWS_ACCESS_KEY_ID = {AWS_ACCESS_KEY_ID}")
-    print(f">>> [DEBUG] AWS_SECRET_ACCESS_KEY starts with = {str(AWS_SECRET_ACCESS_KEY)[:4] if AWS_SECRET_ACCESS_KEY else None}")
     print(">>> [DEBUG] USE_AWS is set. Configuring S3...")
 
     AWS_STORAGE_BUCKET_NAME = 'art-flight-90b83d1ec001'
@@ -206,6 +203,8 @@ if 'USE_AWS' in os.environ:
 
     print(f">>> [DEBUG] Bucket: {AWS_STORAGE_BUCKET_NAME}")
     print(f">>> [DEBUG] Region: {AWS_S3_REGION_NAME}")
+    print(f">>> [DEBUG] AWS_ACCESS_KEY_ID = {AWS_ACCESS_KEY_ID[:4] if AWS_ACCESS_KEY_ID else None}")
+    print(f">>> [DEBUG] AWS_SECRET_ACCESS_KEY starts with = {AWS_SECRET_ACCESS_KEY[:4] if AWS_SECRET_ACCESS_KEY else None}")
     print(f">>> [DEBUG] STATICFILES_LOCATION = 'static'")
     print(f">>> [DEBUG] MEDIAFILES_LOCATION = 'media'")
 
