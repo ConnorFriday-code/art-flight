1d40b486d93b1c164ea6be0dab5c3a96c8597a93
ConnorFriday-code <connor.friday.cf@gmail.com>
2025-09-16 11:45:42 +0100

    Adding more debug comments to custom_storages.

--- custom_storages.py
+++ custom_storages.py
@@ -3,12 +3,17 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 print(">>> [DEBUG] custom_storages.py loaded")
 
-
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-    print(">>> [DEBUG] StaticStorage initialised with location =", self.location)
+
+    def __init__(self, *args, **kwargs):
+        print(f">>> [DEBUG] StaticStorage initialised with location = {self.location}")
+        super().__init__(*args, **kwargs)
 
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-    print(">>> [DEBUG] MediaStorage initialised with location =", self.location)
+
+    def __init__(self, *args, **kwargs):
+        print(f">>> [DEBUG] MediaStorage initialised with location = {self.location}")
+        super().__init__(*args, **kwargs)
