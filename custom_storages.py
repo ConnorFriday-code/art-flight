877b08a526c029c14630a61dc4040e612c85f870
ConnorFriday-code <connor.friday.cf@gmail.com>
2025-09-16 11:41:38 +0100

    Adding more debug comments to custom_storages.

--- custom_storages.py
+++ custom_storages.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
+print(">>> [DEBUG] custom_storages.py loaded")
+
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-    default_acl = 'public-read'
+    print(">>> [DEBUG] StaticStorage initialised with location =", self.location)
 
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
-    file_overwrite = False
-    default_acl = 'public-read'
+    print(">>> [DEBUG] MediaStorage initialised with location =", self.location)
