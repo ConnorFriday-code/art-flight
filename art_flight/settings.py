d2a9fe1ae3f8aa7552badf809ae1bb8383576503
ConnorFriday-code <connor.friday.cf@gmail.com>
2025-12-10 03:04:49 +0000

    Removing a forgotten debug command.

--- art_flight/settings.py
+++ art_flight/settings.py
@@ -26,11 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# Secret Key from Heroku
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
